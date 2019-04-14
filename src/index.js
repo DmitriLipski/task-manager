c8b8f19643fa3a35f09e9287acaccb993e9514b0
dmitri_lipski <dlipski@klika-tech.com>
2019-04-14 18:36:17 +0300

    Add auth middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// app.use((req, res, next) => {
+//     res.status(503).send("Server is under maintenance mode")
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -13,3 +17,15 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 });
+
+// const jwt = require('jsonwebtoken');
+//
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' });
+//     console.log(token);
+//
+//     const data = jwt.verify(token, 'thisismynewcourse');
+//     console.log(data)
+// };
+
+// myFunction();
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -22,6 +23,10 @@ router.post('/users/login', async (req, res) => {
   } catch (e) {
     res.status(400).send()
   }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user)
 });
 
 router.get('/users', async (req, res) => {
