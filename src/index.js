65da76d6e9b42293f215dcdda99e8b7309fe9318
dmitri_lipski <dlipski@klika-tech.com>
2019-04-14 20:43:48 +0300

    Add auth to task router

--- src/index.js
+++ src/index.js
@@ -18,14 +18,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 });
 
-// const jwt = require('jsonwebtoken');
-//
-// const myFunction = async () => {
-//     const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' });
-//     console.log(token);
-//
-//     const data = jwt.verify(token, 'thisismynewcourse');
-//     console.log(data)
-// };
+const Task = require('./models/task');
+const User = require('./models/user');
 
-// myFunction();
+const main = async () => {
+    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    const user = await User.findById('5cb36abbf7239e7b58c7a746');
+    await user.populate('tasks').execPopulate();
+    console.log(user.tasks)
+};
+
+main();
