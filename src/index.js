501745af02fade24e5194861cff9446c176db56c
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-04-15 21:53:20 +0300

    Add simple uploading files

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use((req, res, next) => {
-//     res.status(503).send("Server is under maintenance mode")
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -18,17 +14,3 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 });
 
-// const Task = require('./models/task');
-// const User = require('./models/user');
-//
-// const main = async () => {
-//     // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-//
-//     const user = await User.findById('5cb36abbf7239e7b58c7a746');
-//     await user.populate('tasks').execPopulate();
-//     console.log(user.tasks)
-// };
-//
-// main();
