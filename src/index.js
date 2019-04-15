5aebefffe217c898787c5452d05ae35cb18b2720
dmitri_lipski <dlipski@klika-tech.com>
2019-04-15 09:25:02 +0300

    Add timestamp to user and task schemas

--- src/index.js
+++ src/index.js
@@ -18,17 +18,17 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 });
 
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner)
-
-    const user = await User.findById('5cb36abbf7239e7b58c7a746');
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks)
-};
-
-main();
+// const Task = require('./models/task');
+// const User = require('./models/user');
+//
+// const main = async () => {
+//     // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+//     // await task.populate('owner').execPopulate()
+//     // console.log(task.owner)
+//
+//     const user = await User.findById('5cb36abbf7239e7b58c7a746');
+//     await user.populate('tasks').execPopulate();
+//     console.log(user.tasks)
+// };
+//
+// main();
